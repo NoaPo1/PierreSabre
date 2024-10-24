a6b586916d7760cf1563060f4d89f28e39d58b8d
unknown <noa.poncet@univ-tlse3.fr>
2024-10-24 17:46:38 +0200

    1.1

--- src/Histoire/HistoireTP4.java
+++ src/Histoire/HistoireTP4.java
@@ -1,5 +1,15 @@
 package Histoire;
-
+import personnages.Humain;
 public class HistoireTP4 {
-
+	public static void main(String[] args) {
+		Humain prof = new Humain("Prof",0,"Bierre");
+		prof.setBoisson("Kombucha");
+		prof.gagnerArgent(54);
+		prof.direBonjour();
+		prof.acheter(12, "boisson");
+		prof.boire();
+		prof.acheter(2,"jeu");
+		prof.acheter(50, "Kimono");
+	}
+	
 }
--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,5 +1,67 @@
 package personnages;
 
 public class Humain {
-
+	
+	private String nom;
+	
+	private int argent;
+	
+	private String boisson;
+	
+	public Humain(String nom, int argent, String boisson) {
+		this.nom = nom;
+		this.argent = argent;
+		this.boisson = boisson;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getArgent() {
+		return argent;
+	}
+	public void setArgent(int argent) {
+		this.argent = argent;
+	}
+	public String getBoisson() {
+		return boisson;
+	}
+	public void setBoisson(String boisson) {
+		this.boisson = boisson;
+	}
+	
+	public void gagnerArgent(int argent) {
+		this.argent = this.argent + argent;
+	}
+	
+	public void perdreArgent(int argent) {
+		this.argent = this.argent - argent;
+	}
+	
+	public void parler(String Texte) {
+		System.out.println(Texte);
+	}
+	
+	public void direBonjour() {
+		parler("Bonjours ! Je m'appelle " + getNom() + "et j'aime boire du " + getBoisson() );
+	}
+	
+	public void boire () {
+		parler("Mmmm, un bon verre de " + getBoisson() + " ! GLOUPS !");
+	}
+	
+	public void acheter(int prix , String objet) {
+		if (getArgent() > prix) {
+			parler("j'ai " + getArgent() + " sous en poche.Je vais pouvoir m'offrir une "+ objet +" à "+ prix +" sous");
+			this.perdreArgent(prix);
+		}
+		else {
+			parler("Je n'ai que "+ getArgent() + " je ne peux pas achet "+ objet + " A "+ prix );
+		}
+			
+	}
 }
+
